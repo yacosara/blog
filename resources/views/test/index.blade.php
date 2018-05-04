3efb2877b4367b99af2b7b486f4d92aff89bee57
david <arriola_david@hotmail.com>
2018-05-04 16:16:32 -0300

    se comitea

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -1,8 +1,14 @@
 
 
 {{ $articulo->title }}
-<br>
-<hr>
+<br><hr>
+{{ $articulo->content }}
+<br><hr>
+{{ $articulo->user->name }}
+<br><hr>
+{{ $articulo->category->name }}
+<br><hr>
+<br><hr>
 {{ $articulo->content }}
 <br><hr>
 {{ $articulo->user->name }}
